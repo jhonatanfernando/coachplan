435d21992a729f6fa62d81b2f0cb4199d794f997
Jhonatan Fernando de Oliveira <jhonatan.oliveira@oneocean.com>
2023-01-29 12:20:01 -0500

    - Added Rate limit configuration.

--- CoachPlan/CoachPlan/Program.cs
+++ CoachPlan/CoachPlan/Program.cs
@@ -1,3 +1,4 @@
+using AspNetCoreRateLimit;
 using CoachPlan.Data.Context;
 using CoachPlan.Data.Repositories;
 using CoachPlan.Domain.Entities;
@@ -5,15 +6,40 @@ using CoachPlan.Domain.Repositories;
 using CoachPlan.Domain.Services;
 using CoachPlan.Features.Muscle.Command;
 using CoachPlan.Features.Muscle.Query;
+using CoachPlan.RateLimiting;
 using CoachPlan.Service.Services;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// needed to store rate limit counters and ip rules
+builder.Services.AddMemoryCache();
+
+//load general configuration from appsettings.json
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+
+//load ip rules from appsettings.json
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+
+// configure client rate limiting middleware
+//builder.Services.Configure<ClientRateLimitOptions>(builder.Configuration.GetSection("ClientRateLimiting"));
+//builder.Services.Configure<ClientRateLimitPolicies>(builder.Configuration.GetSection("ClientRateLimitPolicies"));
+
+
+// inject counter and rules stores    
+builder.Services.AddInMemoryRateLimiting();
+
+// configuration (resolvers, counter key builders)
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+//builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddSingleton<IRateLimitConfiguration, CustomRateLimitConfiguration>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
@@ -25,6 +51,8 @@ builder.Services.AddDbContext<CoachPlanContext>(options =>
 
 var app = builder.Build();
 
+app.UseIpRateLimiting();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -33,6 +61,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/status", () =>
+{
+    return "Running";
+});
 
 app.MapGet("/muscles", async (IMediator _mediator) =>
 {
